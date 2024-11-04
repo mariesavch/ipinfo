a84e0e380b7bedf956620738afc7d11cda0c7270
mariesavch <mariesavch@icloud.com>
2024-11-04 16:10:15 +0500

    tailwind merge

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 
 use dioxus::prelude::*;
 use serde::Deserialize;
+use tailwind_fuse::tw_merge;
 
 const _TAILWIND_URL: &str = manganis::mg!(file("assets/tailwind.css"));
 
@@ -33,6 +34,7 @@ async fn get_ipinfo(ip: String) -> reqwest::Result<IpinfoResponce> {
 fn App() -> Element {
     let mut ip = use_signal(|| "".to_string());
     let ipinfo = use_resource(move || async move { get_ipinfo(ip.to_string()).await });
+    let item = "flex py-3 flex-col gap-1";
 
     rsx! {
         main { class: "mx-auto max-w-[850px] px-6 pb-20",
@@ -45,59 +47,60 @@ fn App() -> Element {
                     r#type: "text",
                     autofocus: true,
                     oninput: move |event| ip.set(event.value()),
-                    class: "rounded-md border border-surface0 bg-base
-                            py-2 px-4 capitalize shadow-sm
-                            outline-none transition-colors duration-300
-                            placeholder:text-overlay0 hover:border-surface1
-                            focus:text-text focus:border-surface2"
+                    class: tw_merge!(
+                        "rounded-md border border-surface0 bg-base py-2 px-4",
+                        "outline-none transition-colors duration-300",
+                        "placeholder:text-overlay0 hover:border-surface1",
+                        "focus:text-text focus:border-surface2"
+                    )
                 }
                 div { class: "mt-6",
                     if let Some(Ok(data)) = ipinfo.read().as_ref() {
                         ul { class: "animated-list grid grid-cols-1 sm:grid-cols-2 mt-5",
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "IP" }
                                     span { "{data.ip}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Hostname" }
                                     span { "{data.hostname}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "City" }
                                     span { "{data.city}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Region" }
                                     span { "{data.region}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Country" }
                                     span { "{data.country}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Location" }
                                     span { "{data.loc}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Org" }
                                     span { "{data.org}" }
                                 }
                             }
                             li {
-                                div { class: "flex py-3 flex-col gap-1",
+                                div { class: item,
                                     span { class: "text-overlay0", "Timezone" }
                                     span { "{data.timezone}" }
                                 }
